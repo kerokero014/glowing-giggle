2c364bfe830fd530441a725eafced8de58bf6514
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-16 14:44:42 -0700

    finally
    Connected to the DB in Mongo

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const nameRoutes = require("./routes/nameRoutes");
-//const mongodb = require("./db/connect");
+const mongodb = require("./db/connect");
 
 
 app.use("/", nameRoutes);
 
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });

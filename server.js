c907e4ba88e2efc65710d100d6ff4cdad83c6097
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-19 15:55:50 -0700

    Update .gitignore and contacts.json, and modify routes and server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
 const port = process.env.PORT || 8080;
-const mongodb = require("./db/connect");
-const bodyParser = require("body-parser");
+const app = express();
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use("/", require("./routes"));
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {

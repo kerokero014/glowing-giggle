e96e962c5d110955d74e5b6a071756db0543ee06
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-19 13:00:49 -0700

    Refactor contacts controller to use single quotes and remove newline at end of file

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,24 +1,23 @@
-const mongodb = require("../db/connect");
-const ObjectId = require("mongodb").ObjectID;
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection("contacts").find();
+  const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
-
 
 const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
     .db()
-    .collection("contacts")
+    .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
 };

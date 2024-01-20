100d0f08c1806d632e061fa968589201ca379359
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-19 22:27:54 -0700

    Refactor contacts controller and routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,24 +2,28 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb.getDb().db().collection('contacts').find().toArray();
+    console.log('Retrieved data:', result); // Log the retrieved data
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error fetching all contacts:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').findOne({ _id: userId });
+    console.log('Retrieved single contact:', result); // Log the retrieved single contact
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error fetching single contact:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 module.exports = { getAll, getSingle };

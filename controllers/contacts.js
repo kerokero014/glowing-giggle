2c4bed2f3273ee06e39e643b96a1829cc5791a73
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-20 18:28:45 -0700

    Refactor code for retrieving contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,32 +1,49 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
   try {
-    const result = await mongodb.getDb().db().collection('contacts').find().toArray();
-    console.log('Retrieved data:', result); // Log the retrieved data
-    res.setHeader('Content-Type', 'application/json');
+    console.log("Before database query");
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .find()
+      .toArray();
+    console.log("After database query, Result:", result);
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(result);
   } catch (error) {
-    console.error('Error fetching all contacts:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching all contacts:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 const getSingle = async (req, res, next) => {
   try {
     const userId = new ObjectId(req.params.id);
-    console.log('userId:', userId); // Log the userId
-    const result = await mongodb.getDb().db().collection('contacts').findOne({ _id: userId });
-    console.log('Retrieved single contact:', result); // Log the retrieved single contact
-    
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
+    console.log("userId:", userId); 
+
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .findOne({ _id: userId });
+
+    console.log("Retrieved single contact:", result); 
+
+    res.setHeader("Content-Type", "application/json");
+
+    if (!result) {
+      console.log("Contact not found for the given ID");
+      res.status(404).json({ error: "Contact not found" });
+    } else {
+      res.status(200).json(result);
+    }
   } catch (error) {
-    console.error('Error fetching single contact:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching single contact:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 module.exports = { getAll, getSingle };
-  
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
 const port = process.env.PORT || 8080;
 const app = express();
 

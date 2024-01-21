b7dd3cff1fead74b15bfc3524de7cc0a43ea1b53
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-20 20:58:54 -0700

    Fix formatting in contacts controller and routes.rest file

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,8 +5,8 @@ const getAll = async (req, res, next) => {
   try {
     const result = await mongodb.getDb().db().collection('contacts').find();
     const lists = await result.toArray();
-    
-    res.setHeader('Content-Type', 'application/json');
+
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   } catch (error) {
     console.error("Error in getAll function:", error);
@@ -22,14 +22,14 @@ const getSingle = async (req, res, next) => {
     const result = await mongodb
       .getDb()
       .db()
-      .collection('contacts')
+      .collection("contacts")
       .find({ _id: userId });
 
     const lists = await result.toArray();
 
-    res.setHeader('Content-Type', 'application/json');
-    console.log('lists:', lists);
-    
+    res.setHeader("Content-Type", "application/json");
+    console.log("lists:", lists);
+
     if (lists.length > 0) {
       res.status(200).json(lists[0]);
     } else {

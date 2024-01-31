a2a382652b2fcd9e13b917510ab9b545b00ea3b0
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-30 17:31:44 -0700

    Add Swagger documentation and dependencies

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const routes = require('./routes');
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
   .use(bodyParser.json())

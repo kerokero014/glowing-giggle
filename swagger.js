e0fa710b1bb406fb22c5618748ae75dc33a8bdb3
kerokero014 <kevinjrm.kr@gmail.com>
2024-01-30 18:30:30 -0700

    Update host in swagger.js file

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:8080'
+  host: 'cse341kjm.onrender.com'
 };
 
 const outputFile = './swagger-output.json';
